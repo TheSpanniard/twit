4311fdc6fd382e35da4c9f4ec820889c0359c2f6
Austin Spann <spanna@Austins-MacBook-Pro-4.local>
2020-05-03 12:05:24 -0700

    User: Initial Commit

--- twit/api/models.py
+++ twit/api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+"""
+User Model
+"""
+class User(models.Model):
+    first_name = models.CharField()
+    last_name = models.CharField()
+    user_name = models.CharField()
+    show_name = models.CharField()
+    password = models.CharField()
+    email = models.CharField()
+    """
+    TODO:
+    List of Followers and Following
+    Posts
+    Messaging
+    Encrypt Password
+    """
